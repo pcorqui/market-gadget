79110cc3253760f1fe4ec881e0ebf3873f29775f
Paul Cortés Quijano <paul.cortes@forteinnovation.mx>
2023-06-18 00:48:43 -0600

    Initial commit

---
+++ src/test/java/com/example/marketgadget/MarketGadgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketgadget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketGadgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
